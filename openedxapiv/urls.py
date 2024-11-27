5aa3afa6fc5e986e57406ce6e1098af953165007
Oluwatobi.onasanya <oluwatobi.onasnaya@imr.ie>
2024-11-27 13:50:29 +0000

    api

--- openedxapiv/urls.py
+++ openedxapiv/urls.py
@@ -1,12 +1,12 @@
 """
 URLs for openedxapiv.
 """
-from django.urls import path  # pylint: disable=unused-import
+from django.urls import path,re_path  # pylint: disable=unused-import
 from django.views.generic import TemplateView  # pylint: disable=unused-import
 from  openedxapiv.simple_list import SimpleGetAPI
 
 urlpatterns = [
     # TODO: Fill in URL patterns and views here.
-    # re_path(r'', TemplateView.as_view(template_name="openedx_custom_api/base.html")),
-    path('api/v1/simple-get-data2', SimpleGetAPI.as_view(), name='simple-get-data2')
+    re_path(r'', TemplateView.as_view(template_name="openedx_custom_api/base.html")),
+    path(r'api/v1/simple-get-data2', SimpleGetAPI.as_view(), name='simple-get-data2')
 ]
