a4a6000ff622b7e1bf1ade4760aa5be6001dfbc3
Oluwatobi.onasanya <oluwatobi.onasnaya@imr.ie>
2024-11-27 16:26:29 +0000

    ch

--- openedxapiv/simple_list.py
+++ openedxapiv/simple_list.py
@@ -2,12 +2,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-
+# http://local.edly.io:8000/openedxapiv/api/v1/simple-get-data2
 class SimpleGetAPI(APIView):
     def get(self, request):
         data = [
             {
                 "id": 1,
+                "name": "Example Configuration",
+                "value": "example_value"
+            },
+            {
+                "id": 2,
                 "name": "Example Configuration",
                 "value": "example_value"
             }
